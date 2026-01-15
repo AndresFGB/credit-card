855c9367f09b233c4415398c6aeab4aa32826ffe
AndrewGarz <andres_garzonb@cun.edu.co>
2026-01-15 17:04:54 -0500

    Front created

---
+++ frontend/front-credit-card/src/App.tsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+
+  return (
+  <div className="min-h-screen flex items-center justify-center bg-red-900">
+      <div className="rounded-xl bg-slate-800 p-8 shadow-xl text-white">
+        <h1 className="text-3xl font-bold mb-2">
+          Credit Card App dfhfgh
+        </h1>
+        <p className="text-slate-400">
+          Tailwind 3.4.17 funcionando 🚀
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
